ebe939f8dafb94c8e925ce8bf6504a9853d85c19
jannatOnu <jannatulononna08@gmail.com>
2024-09-23 23:09:31 +0600

    added the first and second btn DOM

---
+++ js/index.js
@@ -0,0 +1,58 @@
+function getDonateBtn(id){
+    const BalanceBtn = parseFloat(document.getElementById(id).innerText);
+    return BalanceBtn;
+
+}
+function getInputBtn(id){
+    const DonateInput  = parseFloat(document.getElementById(id).value);
+    return DonateInput ;
+
+}
+
+const firstDonateBtn = document.getElementById("donate-btn-1");
+firstDonateBtn.addEventListener('click',function(){
+
+//   const firstDonateInput =parseFloat(document.getElementById("donate-input-1").value);
+  const firstDonateInput =getInputBtn("donate-input-1")
+//   const firstBalanceBtn =parseFloat(document.getElementById("balance-btn-1").innerText);
+  const firstBalanceBtn = getDonateBtn("balance-btn-1")
+//   const mainBalance = parseFloat(document.getElementById("main-balance").innerText);
+  const mainBalance = getDonateBtn("main-balance")
+  if(isNaN(firstDonateInput) || firstDonateInput<=0){
+    alert ("Invalid Amount")
+    return;
+  }
+  else{
+    const firstDonation = firstDonateInput + firstBalanceBtn;
+    const remainingBalance = mainBalance - firstDonateInput
+    document.getElementById("balance-btn-1").innerText = firstDonation.toFixed(2);
+    document.getElementById("main-balance").innerText = remainingBalance.toFixed(2);
+
+  }
+
+})
+
+const secondDonateBtn = document.getElementById("donate-btn-2");
+secondDonateBtn.addEventListener('click',function(){
+
+//   const firstDonateInput =parseFloat(document.getElementById("donate-input-1").value);
+  const secondDonateInput =getInputBtn("donate-input-2")
+//   const firstBalanceBtn =parseFloat(document.getElementById("balance-btn-1").innerText);
+  const secondBalanceBtn = getDonateBtn("balance-btn-2")
+//   const mainBalance = parseFloat(document.getElementById("main-balance").innerText);
+  const mainBalance = getDonateBtn("main-balance")
+  if(isNaN(secondDonateInput) || secondDonateInput<=0){
+    alert ("Invalid Amount")
+    return;
+  }
+  else{
+    const secondDonation = secondDonateInput + secondBalanceBtn;
+    const remainingBalance = mainBalance - secondDonateInput
+    document.getElementById("balance-btn-2").innerText = secondDonation.toFixed(2);
+    document.getElementById("main-balance").innerText = remainingBalance.toFixed(2);
+
+  }
+
+})
+
+
